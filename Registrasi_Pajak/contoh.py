79d85d8bc3ad9a3002620bd379e3c52431342da9
Luigi0511 <67412248+Luigi0511@users.noreply.github.com>
2021-06-17 14:29:02 +0700

    Add files via upload

---
+++ Registrasi_Pajak/contoh.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from tkinter import simpledialog
+
+root = tk.Tk() # Create an instance of tkinter
+
+start_date = simpledialog.askstring(title = "Test Title", prompt = "Entire Start Date in MM/DD/YYYY format:")
+print(start_date)
